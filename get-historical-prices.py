9cc76ca793cd44ffc463fab4ad82e89f7228b1e5
Phil McDaniel <mcdanipc@gmail.com>
2021-05-02 18:16:17 -0500

    Update get-historical-prices.py
    Added dataframe for nasdaq

--- get-historical-prices.py
+++ get-historical-prices.py
@@ -4,7 +4,7 @@ import pandas as pd
 dji = yf.Ticker('^DJI')
 #dji.info
 
-#get all available historical data
+#get all available historical data for dow jones industrial average
 dji_hist_data = dji.history("MAX")
 
 #add date column in because Power BI doesn't take the index
@@ -14,11 +14,10 @@ dji_hist_data['Date'] = dji_hist_data.index
 dji_hist_data['Ticker'] = '^DJI'
 
 
-
 gspc = yf.Ticker('^GSPC')
 #gspc.info
 
-#get all available historical data
+#get all available historical data for s&p
 gspc_hist_data = gspc.history("MAX")
 
 #add date column in because Power BI doesn't take the index
@@ -27,10 +26,22 @@ gspc_hist_data['Date'] = gspc_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 gspc_hist_data['Ticker'] = '^GSPC'
 
+ixic = yf.Ticker('^IXIC')
+#ixic.info
+
+#get all available historical data for nasdaq
+ixic_hist_data = ixic.history("MAX")
+
+#add date column in because Power BI doesn't take the index
+ixic_hist_data['Date'] = ixic_hist_data.index
+
+#add column for ticket to accomodate multiple stocks in same dataframe later
+ixic_hist_data['Ticker'] = '^IXIC'
+
 
 
 #combine dataframes into a single dataframe
-dataframes = [dji_hist_data,gspc_hist_data]
+dataframes = [dji_hist_data,gspc_hist_data,ixic_hist_data]
 
 combined_data = pd.concat(dataframes)
 combined_data
