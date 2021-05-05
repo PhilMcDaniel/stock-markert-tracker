31bead75a40dfb8f194a2e792ae3c03eb0e0c20c
Phil McDaniel <mcdanipc@gmail.com>
2021-05-05 15:31:56 -0500

    Added investment name and quote type
    PCM - Add short name and quote type fields

--- get-historical-prices.py
+++ get-historical-prices.py
@@ -2,14 +2,16 @@ import yfinance as yf
 import pandas as pd
 
 dji = yf.Ticker('^DJI')
-#dji.info
+dji.info
 #get all available historical data for dow jones industrial average
 dji_hist_data = dji.history("MAX")
 #add date column in because Power BI doesn't take the index
 dji_hist_data['Date'] = dji_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 dji_hist_data['Ticker'] = '^DJI'
-
+dji_hist_data['Quote Type'] = dji.info['quoteType']
+dji_hist_data['Short Name'] = dji.info['shortName']
+dji_hist_data['Sector'] = dji.info['sector']
 
 gspc = yf.Ticker('^GSPC')
 #gspc.info
@@ -19,6 +21,9 @@ gspc_hist_data = gspc.history("MAX")
 gspc_hist_data['Date'] = gspc_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 gspc_hist_data['Ticker'] = '^GSPC'
+gspc_hist_data['Quote Type'] = gspc.info['quoteType']
+gspc_hist_data['Short Name'] = gspc.info['shortName']
+gspc_hist_data['Sector'] = gspc.info['sector']
 
 ixic = yf.Ticker('^IXIC')
 #ixic.info
@@ -28,6 +33,9 @@ ixic_hist_data = ixic.history("MAX")
 ixic_hist_data['Date'] = ixic_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 ixic_hist_data['Ticker'] = '^IXIC'
+ixic_hist_data['Quote Type'] = ixic.info['quoteType']
+ixic_hist_data['Short Name'] = ixic.info['shortName']
+ixic_hist_data['Sector'] = ixic.info['sector']
 
 aapl = yf.Ticker('AAPL')
 #aapl.info
@@ -37,15 +45,22 @@ aapl_hist_data = aapl.history("MAX")
 aapl_hist_data['Date'] = aapl_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 aapl_hist_data['Ticker'] = 'AAPL'
+aapl_hist_data['Quote Type'] = aapl.info['quoteType']
+aapl_hist_data['Short Name'] = aapl.info['shortName']
+aapl_hist_data['Sector'] = aapl.info['sector']
 
 amzn = yf.Ticker('AMZN')
-#aapl.info
+amzn.info
 #get all available historical data for amazon
 amzn_hist_data = amzn.history("MAX")
 #add date column in because Power BI doesn't take the index
 amzn_hist_data['Date'] = amzn_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 amzn_hist_data['Ticker'] = 'AMZN'
+amzn_hist_data['Quote Type'] = amzn.info['quoteType']
+amzn_hist_data['Short Name'] = amzn.info['shortName']
+amzn_hist_data['Sector'] = amzn.info['sector']
+
 
 goog = yf.Ticker('GOOG')
 #goog.info
@@ -55,6 +70,9 @@ goog_hist_data = goog.history("MAX")
 goog_hist_data['Date'] = goog_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 goog_hist_data['Ticker'] = 'GOOG'
+goog_hist_data['Quote Type'] = goog.info['quoteType']
+goog_hist_data['Short Name'] = goog.info['shortName']
+goog_hist_data['Sector'] = goog.info['sector']
 
 msft = yf.Ticker('MSFT')
 #msft.info
@@ -64,6 +82,9 @@ msft_hist_data = msft.history("MAX")
 msft_hist_data['Date'] = msft_hist_data.index
 #add column for ticket to accomodate multiple stocks in same dataframe later
 msft_hist_data['Ticker'] = 'MSFT'
+msft_hist_data['Quote Type'] = msft.info['quoteType']
+msft_hist_data['Short Name'] = msft.info['shortName']
+msft_hist_data['Sector'] = msft.info['sector']
 
 #combine dataframes into a single dataframe
 dataframes = [dji_hist_data,gspc_hist_data,ixic_hist_data,aapl_hist_data,amzn_hist_data,goog_hist_data,msft_hist_data]
