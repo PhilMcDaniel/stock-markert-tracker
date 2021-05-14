f54e022e9e8b16619519aa34f9f394ae74ec2599
Phil McDaniel <mcdanipc@gmail.com>
2021-05-14 11:45:41 -0500

    Update get-historical-prices.py
    Updates to loop instead of repeat code

--- get-historical-prices.py
+++ get-historical-prices.py
@@ -1,93 +1,35 @@
 import yfinance as yf
 import pandas as pd
 
-dji = yf.Ticker('^DJI')
-dji.info
-#get all available historical data for dow jones industrial average
-dji_hist_data = dji.history("MAX")
-#add date column in because Power BI doesn't take the index
-dji_hist_data['Date'] = dji_hist_data.index
-#add column for ticket to accomodate multiple stocks in same dataframe later
-dji_hist_data['Ticker'] = '^DJI'
-dji_hist_data['Quote Type'] = dji.info['quoteType']
-dji_hist_data['Short Name'] = dji.info['shortName']
-dji_hist_data['Sector'] = dji.info['sector']
+ticker_list = ['^DJI','^GSPC','^IXIC','AAPL','AMZN','GOOG','MSFT','VOO','VTI','QQQM','QQQ','VXUS','NVDA','AMD','PLTR','INTC','DIS','CVX','TSLA','SQ','BA','GE','IBM','NKE','SBUX','FB','NFLX']
 
-gspc = yf.Ticker('^GSPC')
-#gspc.info
-#get all available historical data for s&p
-gspc_hist_data = gspc.history("MAX")
-#add date column in because Power BI doesn't take the index
-gspc_hist_data['Date'] = gspc_hist_data.index
-#add column for ticket to accomodate multiple stocks in same dataframe later
-gspc_hist_data['Ticker'] = '^GSPC'
-gspc_hist_data['Quote Type'] = gspc.info['quoteType']
-gspc_hist_data['Short Name'] = gspc.info['shortName']
-gspc_hist_data['Sector'] = gspc.info['sector']
+#initialize combined data frame to solve Power BI error
+combined_data = pd.DataFrame()
+#loop through list of tickers to create and enhance historical data dataframe
+for ticker in ticker_list:
+    #print(ticker)
+    stock = yf.Ticker(ticker)
+    #dji.info
+    
+    #get all available historical data for dow jones industrial average
+    stock_hist_data = stock.history("MAX")
+    
+    #add date column in because Power BI doesn't take the index
+    stock_hist_data['Date'] = stock_hist_data.index
+    
+    #add column for ticket to accomodate multiple stocks in same dataframe later
+    stock_hist_data['Ticker'] = ticker
+    
+    #add more additional columns for quotetype, stock name, and sector
+    stock_hist_data['Quote Type'] = stock.info['quoteType']
+    stock_hist_data['Short Name'] = stock.info['shortName']
+    if 'sector' in stock.info:
+        stock_hist_data['Sector'] = stock.info['sector']
+    else:
+        stock_hist_data['Sector'] = ''
+  
 
-ixic = yf.Ticker('^IXIC')
-#ixic.info
-#get all available historical data for nasdaq
-ixic_hist_data = ixic.history("MAX")
-#add date column in because Power BI doesn't take the index
-ixic_hist_data['Date'] = ixic_hist_data.index
-#add column for ticket to accomodate multiple stocks in same dataframe later
-ixic_hist_data['Ticker'] = '^IXIC'
-ixic_hist_data['Quote Type'] = ixic.info['quoteType']
-ixic_hist_data['Short Name'] = ixic.info['shortName']
-ixic_hist_data['Sector'] = ixic.info['sector']
-
-aapl = yf.Ticker('AAPL')
-#aapl.info
-#get all available historical data for apple
-aapl_hist_data = aapl.history("MAX")
-#add date column in because Power BI doesn't take the index
-aapl_hist_data['Date'] = aapl_hist_data.index
-#add column for ticket to accomodate multiple stocks in same dataframe later
-aapl_hist_data['Ticker'] = 'AAPL'
-aapl_hist_data['Quote Type'] = aapl.info['quoteType']
-aapl_hist_data['Short Name'] = aapl.info['shortName']
-aapl_hist_data['Sector'] = aapl.info['sector']
-
-amzn = yf.Ticker('AMZN')
-amzn.info
-#get all available historical data for amazon
-amzn_hist_data = amzn.history("MAX")
-#add date column in because Power BI doesn't take the index
-amzn_hist_data['Date'] = amzn_hist_data.index
-#add column for ticket to accomodate multiple stocks in same dataframe later
-amzn_hist_data['Ticker'] = 'AMZN'
-amzn_hist_data['Quote Type'] = amzn.info['quoteType']
-amzn_hist_data['Short Name'] = amzn.info['shortName']
-amzn_hist_data['Sector'] = amzn.info['sector']
-
-
-goog = yf.Ticker('GOOG')
-#goog.info
-#get all available historical data for amazon
-goog_hist_data = goog.history("MAX")
-#add date column in because Power BI doesn't take the index
-goog_hist_data['Date'] = goog_hist_data.index
-#add column for ticket to accomodate multiple stocks in same dataframe later
-goog_hist_data['Ticker'] = 'GOOG'
-goog_hist_data['Quote Type'] = goog.info['quoteType']
-goog_hist_data['Short Name'] = goog.info['shortName']
-goog_hist_data['Sector'] = goog.info['sector']
-
-msft = yf.Ticker('MSFT')
-#msft.info
-#get all available historical data for amazon
-msft_hist_data = msft.history("MAX")
-#add date column in because Power BI doesn't take the index
-msft_hist_data['Date'] = msft_hist_data.index
-#add column for ticket to accomodate multiple stocks in same dataframe later
-msft_hist_data['Ticker'] = 'MSFT'
-msft_hist_data['Quote Type'] = msft.info['quoteType']
-msft_hist_data['Short Name'] = msft.info['shortName']
-msft_hist_data['Sector'] = msft.info['sector']
-
-#combine dataframes into a single dataframe
-dataframes = [dji_hist_data,gspc_hist_data,ixic_hist_data,aapl_hist_data,amzn_hist_data,goog_hist_data,msft_hist_data]
-
-combined_data = pd.concat(dataframes)
+    #stock_hist_data
+    combined_data = combined_data.append(stock_hist_data)
+    
 combined_data
